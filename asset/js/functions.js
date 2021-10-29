37f7878247fd42d03c4f747ac9a5629da5cee281
mskanneh <mskanneh@gmail.com>
2021-10-29 18:39:13 +0000

    Initial upload of KVI source codes

---
+++ asset/js/functions.js
@@ -0,0 +1,30 @@
+
+// Function without return statement
+function salutation (name) {
+    console.log ('Hello ' + name);
+    }
+   salutation ('Matenneh Dorley, my lovely mom'); // Hello Jamielatou
+    
+// Function with return statement
+function greetings (name) {
+    return 'Hello Jamielatou B Kanneh, my lovely wife ';
+        }
+        
+    let message = greetings();
+    console.log(message);
+        
+// Function with Arguments
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+let result = sum(2, 3);
+console.log(result); // 5
+
+function greetings(name) {
+    console.log(`Hello ${name}`);
+}
+
+greetings('Oldma Tanue');
+greetings('Jamielatou');
