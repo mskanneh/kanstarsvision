1df4c9790a27137d7aa158a448d5dac0a6ec6506
mskanneh <mskanneh@gmail.com>
2021-10-30 10:41:24 +0000

    Update to functions.js

--- asset/js/functions.js
+++ asset/js/functions.js
@@ -28,3 +28,116 @@ function greetings(name) {
 
 greetings('Oldma Tanue');
 greetings('Jamielatou');
+
+// The Arguments Object
+
+function printAll(){
+    for(let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+}
+
+printAll(1,2,3,4,5); //1 2 3 4 5
+printAll(10, 20); // 10 20
+
+
+//Functions Scope
+
+function greeting() {
+    let message = 'Hello';
+}
+
+greeting();
+
+console.log(message); //ReferenceError; message is not defined
+
+
+function greetin(){
+    let message = 'I love my wife';
+    let sayHi = function hi(){
+        console.log(message);
+    };
+    sayHi(); // Hello
+}
+
+greetin();
+
+
+function greeti(){
+    let message = 'I love my wife';
+    let sayHi = function hi(){
+        message = 'and my wife loves me';
+    };
+    sayHi(); // Hello
+    console.log(message);
+}
+
+greeti();
+
+//Block Scope
+
+/* Variables declared with the var keyword or within function declarations DO NOT have block scope */
+
+let message1 = 'Hello';
+if (message1 === 'Good day') {
+    let count = 100;
+}
+
+// console.log(count); //Error
+
+
+let message2 = 'Message2';
+if(message2 === 'Message2'){
+    let message2 = 'Inside if block';
+    console.log(message2); // Inside if block
+}
+
+console.log(message2); // Message2
+
+//Self executing or Immediately Invoked Function Expression(IIFE)
+
+(function (){
+    console.log("Good Friday");
+}
+
+
+
+)();
+
+//Closures
+
+let message3 = (function(){
+
+    let message4 = 'I love Programming';
+
+    let getMessage = function() {
+        return message4;
+    };
+       
+// Closure starts here
+return {
+  myMessage: getMessage,
+};//Closure ends here
+
+})();
+console.log(message3.myMessage()); //I love Programming
+
+
+//Another example of Closure
+
+function setupCounter(val){
+    return function counter(){
+        return val++;
+    }
+}
+
+let counter1 = setupCounter(0);
+
+console.log(counter1()); //0
+console.log(counter1()); //1
+
+let counter2 = setupCounter(10);
+
+console.log(counter2()); //10
+console.log(counter2()); //11
+
